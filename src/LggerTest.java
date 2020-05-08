cc9bdb829621253e66edc830aa20a65db1d5974d
matsushita <matsushita@aisl.cs.tut.ac.jp>
2020-05-08 14:16:46 +0900

    [upadte]out put logs

--- src/LggerTest.java
+++ src/LggerTest.java
@@ -1,9 +1,23 @@
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Field;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.impl.SimpleLogger;
 
 public class LggerTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().toString()+".class");
+	
+	
+	public static void main(String[] args){
+		System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "trace");
+		System.out.println(logger.isTraceEnabled()+logger.ROOT_LOGGER_NAME);		
+		
+		
+		logger.trace("TRACE");
+		logger.debug("DEBUG");
+		logger.info("INFO");
+		logger.warn("WARN");
+		logger.error("ERROR");
 	}
-
 }
