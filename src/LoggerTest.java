4e755dbb23ba438a5107a44536ce7a61ecf0bd5e
matsushita <matsushita@aisl.cs.tut.ac.jp>
2020-05-08 14:42:00 +0900

    [mod] add comments and delete unused code

--- src/LoggerTest.java
+++ src/LoggerTest.java
@@ -1,6 +1,4 @@
 import java.lang.invoke.MethodHandles;
-import java.lang.reflect.Field;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.impl.SimpleLogger;
@@ -9,8 +7,8 @@ public class LoggerTest {
 	private static Logger logger;
 	
 	public static void main(String[] args){
-		System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "debug");
-		logger = LoggerFactory.getLogger(MethodHandles.lookup().toString()+".class");
+		System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, args[0]);	//コマンドライン引数に応じてログレベル変更
+		logger = LoggerFactory.getLogger(MethodHandles.lookup().toString()+".class");	//ログレベル変更後にオブジェクトを生成しないと変更が適用されない
 		
 		logger.trace("TRACE");
 		logger.debug("DEBUG");
